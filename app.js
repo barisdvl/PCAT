b73b49df717a9f12c770710c092726fcfe4475de
barisdvl <barisdeveli48@gmail.com>
2022-01-04 09:03:27 +0300

    Heroku Deployment Stage

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 
 const ejs = require('ejs');
 
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost/pcat-test-db')
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -17,27 +20,23 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos
-  });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
